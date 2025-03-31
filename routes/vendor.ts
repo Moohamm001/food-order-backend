06f3f4f25c4050de33cdb02174d250ec3aa5c32e
Moohamm001 <mooham.00771@gmail.com>
2025-03-31 22:28:41 +0700

    update utility

--- routes/vendor.ts
+++ routes/vendor.ts
@@ -13,8 +13,10 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({ messageFromVendor: "Vendor Home Page" });
 });
 
-router.use(Authenticate);
 router.post("/login", VendorLogin);
+
+// use Authenticate middleware for all routes below
+router.use(Authenticate);
 router.get("/profile", GetVendorProfile);
 router.patch("/profile", UpdateVendorProfile);
 router.patch("/service", UpdateVendorService);
--- utils/tokenHelper.ts
+++ utils/tokenHelper.ts
@@ -1,20 +1,32 @@
-import { Request } from 'express';
-import jwt from 'jsonwebtoken';
-import { VendorPayload } from '../dto';
-import { APP_SECRET } from '../config';
+import { Request } from "express";
+import jwt from "jsonwebtoken";
+import { VendorPayload } from "../dto";
+import { APP_SECRET } from "../config";
 
 export const generateToken = (payload: VendorPayload) => {
-    return jwt.sign(payload, APP_SECRET, { expiresIn: '1h' });
-}
-
+  return jwt.sign(payload, APP_SECRET, { expiresIn: "1h" });
+};
 
 export const verifyToken = async (req: Request) => {
-    const signedToken = req.headers['authorization'];
-    
-    if ( signedToken ){
-        const payload = await jwt.verify(signedToken.split(' ')[1], APP_SECRET) as VendorPayload;
-
+  try {
+    const header = req.headers["authorization"];
+    // console.log("signedToken", header);
+    if (header) {
+      // get last for token
+      const token = header.split(" ") || [];
+      const jasonToken = token[token.length - 1];
+      if (token) {
+        const payload = (await jwt.verify(
+          jasonToken,
+          APP_SECRET
+        )) as VendorPayload;
         req.user = payload;
         return true;
+      }
     }
-}
+  } catch (error) {
+    console.log("error", error);
+    return false;
+  }
+  return false;
+};
